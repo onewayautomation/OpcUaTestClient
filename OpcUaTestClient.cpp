c593de2873b83b1cf8d4486fd49c67daf9c8c457
RavilN <ravil@onewayautomation.com>
2018-07-19 01:04:56 -0600

    Added step connection->shutdown, which was added in the latest commit of the SDK.

--- OpcUaTestClient.cpp
+++ OpcUaTestClient.cpp
@@ -221,6 +221,9 @@ int main(int argc, char* argv[])
 			}
 
 			connectResult = connection->disconnect(true).get();
+
+			// Calling shutdown method guarantees that no callbacks are made from background thread.
+			connection->shutdown();
 		}
 	}
 	Utils::closeSdk();
